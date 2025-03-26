32d83079cab5c67d60550af29cdd02c1c3ac6e6b
m0nkeybus1ness <145599507+m0nkeybus1ness@users.noreply.github.com>
2025-03-26 16:03:27 -0500

    Update Integration Function.py
    This adds comments and adds a few minor tweaks to the original

--- Integration Function.py
+++ Integration Function.py
@@ -1,3 +1,9 @@
+"""
+This is a program to calculate the integral of a square function using
+ two methods. The goal is for the result to have a margin error of
+  0.001.These assume a lower bound of 0 and a custom upper bound.
+ """
+# The first method is using the Riemann sum to find the integral
 def integrate(function,upperBound,stepSize = 0.0001):
     integral = 0
     x = 0
@@ -7,9 +13,11 @@ def integrate(function,upperBound,stepSize = 0.0001):
 
     return integral
 
+# This method creates the function x^2
 def square_function(x):
     return x ** 2
 
+# This method calculates the integral using the trapezoidal method
 def trapeziodal(function,upperBound,stepSize = 0.001):
     integral = 0
     x = 0
@@ -19,5 +27,6 @@ def trapeziodal(function,upperBound,stepSize = 0.001):
         x += stepSize
     return integral
 
+# These lines find the integral of x^2 with a lower bound of 0 and upper bound of 5
 print(integrate(square_function,5))
 print(trapeziodal(square_function,5))
